c85f24ed577dc9cd9ba1b10fec92604d4640067a
Lloyd Headbush <ocanica@gmail.com>
2019-01-04 19:03:25 +0000

    Added CityDto and local data store

---
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesRepository.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesRepository.Current.Cities.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
---
+++ CityInfo/CityInfo.API/Models/CitiesRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Models
+{
+    public class CitiesRepository
+    {
+        public List<CityDto> Cities;
+        public static CitiesRepository Current { get; } = new CitiesRepository();
+
+        public CitiesRepository()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "London",
+                    Description = "The one with Big Ben"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "New York City",
+                    Description = "The one with the Skyscrapers"
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Cape Town",
+                    Description = "The one with the flat Mountain"
+                }
+            };
+        }
+    }
+}
