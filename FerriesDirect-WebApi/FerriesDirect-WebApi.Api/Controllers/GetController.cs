69503e13dae9baed6166dd225e6a7e7d372a6b0b
Lloyd Headbush <ocanica@gmail.com>
2020-02-21 12:11:50 +0000

    Added new ApiRoutes/Contract

---
+++ FerriesDirect-WebApi/FerriesDirect-WebApi.Api/Controllers/GetController.cs
@@ -0,0 +1,23 @@
+﻿using FerriesDirect_WebApi.Api.Contracts.V1;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FerriesDirect_WebApi.Api.Controllers
+{
+    public class GetController : Controller
+    {
+        /// <summary>
+        /// GET api/v1/get
+        /// This returns all the information from the current end-point 
+        /// </summary>
+        [HttpGet(ApiRoutes.Items.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ FerriesDirect-WebApi/FerriesDirect-WebApi.Api/Models/PersonDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FerriesDirect_WebApi.Api.Models
+{
+    public class PersonDto
+    {
+        [JsonPropertyName("FirstName")]
+        public string FirstName { get; set; }
+        [JsonPropertyName("Surname")]
+        public string Surname { get; set; }
+        [JsonPropertyName("Score")]
+        public int Score { get; set; }
+    }
+}
