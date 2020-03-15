04c1301dd1cdd8d347009ebfc0361272e16524d0
Lloyd Headbush <ocanica@gmail.com>
2020-03-15 20:27:31 +0000

    Added connection string to appsetting, issues with SqlServer package

---
+++ SimpleUsers-WebApi/SimpleUsers-WebApi/Data/UsersDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleUsers_WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleUsers_WebApi.Data
+{
+    public class UsersDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UsersDbContext(DbContextOptions<UsersDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
